1a7fce5c0b50b2bd17b4182f02b8359e695070da
Anton Lindström <lindztr@gmail.com>
2012-08-17 11:27:41 +0200

    remove some extra conditionals

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -3,65 +3,35 @@ define munin::plugin(
     $script_path = '',
     $config      = '')
 {
-    $plugin      = "/etc/munin/plugins/${name}"
-    $plugin_conf = "/etc/munin/plugin-conf.d/${name}.conf"
+  $plugin      = "/etc/munin/plugins/${name}"
+  $plugin_conf = "/etc/munin/plugin-conf.d/${name}.conf"
 
-    if $script_path {
-        $script_dir = $script_path
+  if $script_path {
+    $script_dir = $script_path
+  } else {
+    $script_dir = $::operatingsystem ? {
+      'Archlinux' => '/usr/lib/munin/plugins',
+      default     => '/usr/share/munin/plugins',
     }
-    else {
-        $script_dir = $::operatingsystem ? {
-            'Archlinux' => '/usr/lib/munin/plugins',
-            default     => '/usr/share/munin/plugins',
-        }
-    }
+  }
 
-    case $ensure {
-        'absent': {
-            file { $plugin: 
-                ensure => $ensure,
-            }
-        }
+  $ensure_link = $ensure ? {
+    present => link,
+    default => $ensure,
+  }
 
-        default: {
-            $ensure_link = $ensure ? {
-                present => link,
-                default => $ensure,
-            }
+  file { $plugin:
+    ensure  => $ensure_link,
+    target  => "${script_dir}/${name}",
+    require => Class['munin::package'],
+    notify  => Class['munin::service'],
+  }
 
-            file { $plugin:
-                ensure  => $ensure_link,
-                target  => "${script_dir}/${name}",
-                require => Class['munin::package'],
-                notify  => Class['munin::service'],
-            }
-        }
-    }
-
-    case $config {
-        '': {
-            file { $plugin_conf:
-                ensure => absent,
-            }
-        }
-
-        default: {
-            case $ensure {
-                'absent': {
-                    file { $plugin_conf:
-                        ensure => $ensure,
-                    }
-                }
-
-                default: {
-                    file { $plugin_conf: 
-                        content => "[${name}]\n${config}",
-                        mode    => 0644,
-                        owner   => root,
-                        group   => root,
-                    }
-                }
-            }
-        }
-    }
+  file { $plugin_conf:
+    ensure  => $ensure,
+    content => "[${name}]\n${config}",
+    mode    => '0644',
+    owner   => root,
+    group   => root,
+  }
 }
