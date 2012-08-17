67c944b2dca5d89a0bd0ebd6d2d3005f5a1bc7f7
Anton Lindström <lindztr@gmail.com>
2012-08-17 12:00:31 +0200

    add spec for plugin.pp

---
+++ spec/defines/munin__plugin_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe 'munin::plugin', :type => :define do
+  let(:title) { 'cpu' }
+
+  describe 'plugin cpu on ArchLinux' do
+    let(:facts) { {:operatingsystem => 'ArchLinux' } }
+
+    it do should contain_file('/etc/munin/plugins/cpu').with( 
+      'ensure'  => 'link',
+      'target'  => '/usr/lib/munin/plugins/cpu'
+    ) end
+      
+  end
+
+  describe 'plugin cpu on Ubuntu' do
+    let(:facts) { {:operatingsystem => 'Ubuntu' } }
+
+    it do should contain_file('/etc/munin/plugins/cpu').with( 
+      'ensure'  => 'link',
+      'target'  => '/usr/share/munin/plugins/cpu'
+    ) end
+      
+  end
+
+  describe 'specific content' do
+    let(:params) { { :config => 'test' } }
+    it 'should include plugin cpu' do
+      content = catalogue.resource('file', '/etc/munin/plugin-conf.d/cpu.conf').send(:parameters)[:content]
+      content.should match /cpu/
+    end
+  end
+
+  describe 'ensure absent' do
+    let(:params) { { :ensure => 'absent' } }
+
+    it { should contain_file('/etc/munin/plugins/cpu').with_ensure('absent') }
+    it { should contain_file('/etc/munin/plugin-conf.d/cpu.conf').with_ensure('absent') }
+  end
+
+end
